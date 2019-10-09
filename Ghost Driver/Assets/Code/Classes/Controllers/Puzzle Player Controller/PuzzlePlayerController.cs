2240baa0aa2a2c0dc1d11cf6ab91012d67d371d1
James Johnson <music1384@yahoo.co.uk>
2019-10-09 21:30:59 +0100

    Implement basic puzzle player controller + camera

--- Ghost Driver/Assets/Code/Classes/Controllers/Puzzle Player Controller/PuzzlePlayerController.cs
+++ Ghost Driver/Assets/Code/Classes/Controllers/Puzzle Player Controller/PuzzlePlayerController.cs
@@ -2,6 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent (typeof (Collider), typeof (Rigidbody))]
+public class PuzzlePlayerController : MonoBehaviour
 {
+    [Tooltip ("The speed at which the player moves.")]
+    [SerializeField] private float _MovementSpeed = 5.0f;
+
+    private bool _CanMove = true;
+    private Vector3 _Direction = Vector3.zero;
+    private Vector3 _SpawnPosition = Vector3.zero;
+    private Rigidbody _Rigidbody = null;
+
+    protected void Awake ()
+    {
+        AssignReferences ();
+        SetupRigidbody ();
+    }
+
+    private void AssignReferences ()
+    {
+        _Rigidbody = GetComponent<Rigidbody> ();
+        GetComponent<Collider> ().isTrigger = true;
+
+        _SpawnPosition = _Rigidbody.position;
+    }
+
+    private void SetupRigidbody ()
+    {
+        _Rigidbody.isKinematic = true;
+        _Rigidbody.useGravity = false;
+        _Rigidbody.freezeRotation = true;
+    }
+
+    public void Update ()
+    {
+        if (_CanMove)
+        {
+            GetInput ();
+            CheckPosition ();
+        }
+    }
+
+    private void GetInput ()
+    {
+        if (_Direction == Vector3.zero)
+        {
+            if (Input.GetKeyDown (KeyCode.W))
+            {
+                _Direction = Vector3.up;
+                //_Rigidbody.rotation = Quaternion.Euler (Vector3.zero);
+            }
+            else if (Input.GetKeyDown (KeyCode.S))
+            {
+                _Direction = Vector3.down;
+                //_Rigidbody.rotation = Quaternion.Euler (new Vector3 (0, 0, 180f));
+            }
+            else if (Input.GetKeyDown (KeyCode.D))
+            {
+                _Direction = Vector3.right;
+                //_Rigidbody.rotation = Quaternion.Euler (new Vector3 (0, 0, 90f));
+            }
+            else if (Input.GetKeyDown (KeyCode.A))
+            {
+                _Direction = Vector3.left;
+                //_Rigidbody.rotation = Quaternion.Euler (new Vector3 (0, 0, 270f));
+            }
+        }
+    }
+
+    private void FixedUpdate ()
+    {
+        Move ();
+    }
+
+    private void Move ()
+    {
+        if (_Direction != Vector3.zero)
+            _Rigidbody.MovePosition (_Rigidbody.position + _Direction * _MovementSpeed * Time.fixedDeltaTime);
+    }
+
+    private void CheckPosition ()
+    {
+        float rayDistance = 0.5f;
+        var ray = new Ray (_Rigidbody.position, _Direction);
+
+        if (Physics.Raycast (ray, out RaycastHit hit, rayDistance))
+        {
+            if (hit.collider.CompareTag ("Finish"))
+            {
+                _Direction = Vector3.zero;
+                _Rigidbody.position = new Vector3 (Mathf.RoundToInt (_Rigidbody.position.x), Mathf.RoundToInt (_Rigidbody.position.y), 0.0f);
+            }
+        }
+    }
 }
---
+++ Ghost Driver/Assets/Code/Classes/Controllers/Puzzle Player Controller/SmoothLookAt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SmoothLookAt : MonoBehaviour
+{
+    [Tooltip ("The target object to look at.")]
+    public Transform _Target;
+    [Tooltip ("Should the camera smooth to look at the target?")]
+    public bool _ShouldSmooth = true;
+    [Tooltip ("The amount of smoothing to apply to the tracking.")]
+    public float _SmoothFactor = 6.0f;
+
+    private Transform _Transform;
+
+    void Awake ()
+    {
+        _Transform = GetComponent<Transform> ();
+    }
+
+    void FixedUpdate ()
+    {
+        if (_Target != null)
+        {
+            if (_ShouldSmooth)
+                SmoothLook ();
+            else
+                Look ();
+        }
+    }
+
+    private void Look ()
+    {
+        var targetPosition = new Vector3 (_Target.position.x, _Target.position.y, _Target.position.z);
+        Vector3 targetDirection = (targetPosition - _Transform.position).normalized;
+
+        _Transform.rotation = Quaternion.FromToRotation (-Vector3.forward, targetDirection);
+    }
+
+    private void SmoothLook ()
+    {
+        var rotation = Quaternion.LookRotation (_Target.position - _Transform.position);
+        _Transform.rotation = Quaternion.Slerp (_Transform.rotation, rotation, Time.deltaTime * _SmoothFactor);
+    }
+}
